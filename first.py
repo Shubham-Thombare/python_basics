f27c215d82e50051652c7be2d028e02283e8e6c5
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-06-15 13:21:09 +0530

    new to github and added first.py and second.py files

---
+++ first.py
@@ -0,0 +1,4 @@
+# This is my first program
+print("myself shubham")
+print("I Like exercising")
+print("I Love Coding")
---
+++ second.py
@@ -0,0 +1,36 @@
+#strings
+first_name="Shubham"
+fruit_name="Mango"
+
+
+
+# integer
+age=19
+quantity=5
+no_of_students=45
+
+#float
+price=10.99
+base_speed=1.3
+
+
+#boolean true/false
+is_student=True
+
+if is_student:
+ print("you are a student")
+
+else:
+  print("you are NOT a student")
+
+
+
+print(f"Hello your name={first_name}")
+print(f"fruit={fruit_name}")
+
+print(f"Your age={age}")
+print(f"Quantity required={quantity}")
+print(f"no_of_studentd={no_of_students}")
+
+print(f"price={price}")
+print(f"speed={base_speed}ghz")
