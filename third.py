9b77a84224f4754cff055769dd49e167f614517b
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-06-19 20:02:46 +0530

    Create third.py
    since last three days i was on small vacation so i was not able to upload and to do coding part but today i return and started with new enjoyment

---
+++ third.py
@@ -0,0 +1,22 @@
+#Typecasting 
+name="shubham"
+age=19
+cet_percentile=90.93
+is_student=True
+
+
+#conversion of float to int
+cet_percentile=int(cet_percentile)
+print(cet_percentile)
+
+#conversion of str to bool
+name=bool(name)
+print(name)
+
+#conversion of bool to float
+is_student=float(is_student)
+print(is_student)
+
+#conversion of int to str
+age=str(age)
+print(age)
