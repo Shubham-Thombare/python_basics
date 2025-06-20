cc1dab58d90513cb58bfd13f3fcc5ddf68ef73dc
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-06-20 11:50:02 +0530

    fourth file with two new exercise
    it takes time to understand but at the end understands clearly.

---
+++ fourth.py
@@ -0,0 +1,10 @@
+#input()
+name=input ("what is your name?:")
+age=int(input ("How old are you?:"))
+
+age +=1
+
+print(f"hello {name}!")
+print("happy birthday")
+print(f"you are {age} years old")
+
