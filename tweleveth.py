67ddb6fb91cb889c5425e52896db5beb6297fce7
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-06-29 12:43:57 +0530

    12th.py
    Unstoppable!!!

---
+++ tweleveth.py
@@ -0,0 +1,14 @@
+# Conditional Operators (ternary operator) - (X if condition else Y)=(formula)
+num = 5
+a = 6
+b = 7
+age = 2
+temp = 27
+#print("Positive" if num > 0 else "Negative") 
+#result = "Even" if num % 2 == 0 else "ODD"
+#max_num = a if a > b else b
+#min_num = a if a < b else b
+#status = "Adult" if age >=25 else "Child"
+
+temp = "favourable" if temp <= 28 else "unfavourable"
+print(temp)
