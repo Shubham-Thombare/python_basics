8bd8cddcf7f66afd5179c1f148534b7697b55563
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-03 11:31:47 +0530

    consistency.
    Thanks to Atomic Habits..

---
+++ fifteenth.py
@@ -0,0 +1,9 @@
+# Format specifiers
+
+price1 = 3.15179
+price2 = -98745.55465
+price3 = 35456.34778
+
+print(f"price 1 is ${price1:,.2f}")
+print(f"price 2 is ${price2:,.2f}")
+print(f"price 3 is ${price3:,.2f}")
