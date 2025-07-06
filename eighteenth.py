a52cc42acfea458be9faabcec15c5f0c6eebd657
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-06 11:58:42 +0530

    there we go

---
+++ eighteenth.py
@@ -0,0 +1,11 @@
+# for loops
+
+#credit_card = "1234-5678-9012-3456"
+#for x in credit_card:
+ # print(x)
+
+for x in range(1, 210000):
+    if x == 12:
+        continue
+    else:
+        print(x)
