55e90f5f6a08a7fd89663e303fce5ed8aa52a080
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-10 12:31:03 +0530

    A big one
    long way to go

---
+++ 21st.py
@@ -0,0 +1,63 @@
+# collections
+#List
+#Set
+#Tuple
+
+
+# Collections
+
+
+# fruits = ["orange", "apple", "banana", "mango"]  #(main)
+# print(dir(fruits))
+# print(len(fruits))
+# print("apple" in fruits)  #(in - operator)
+
+
+#List 
+
+
+fruits = ["orange", "apple", "banana", "mango"]  #(main)
+# fruits.append("pineapple")
+# fruits.remove("apple")
+# fruits.insert(0, "pineapple")
+# fruits.sort()
+# fruits.reverse()
+# fruits.clear()
+#print(fruits.index("apple"))
+# print(fruits.count("banana"))
+
+#fruits[0] = "pineapple"
+
+ # print(fruits[0])
+for fruit in fruits:
+  #print(fruits)   #(main)
+ print(fruit)
+
+# Set
+
+
+# fruits = {"orange", "apple", "banana", "mango"}  #(main)
+# print(dir(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+
+# fruits.add("coconut")
+# fruits.remove("apple")
+# fruits.pop()
+# fruits.clear()
+
+#print(fruits) 
+
+
+# Tuple
+# fruits = ("orange", "apple", "banana", "mango", "coconut")  #(main)
+# print(dir(fruits))
+# print(len(fruits))
+# print("apple" in fruits)
+
+#print(fruits.index("apple"))
+#print(fruits.count("banana"))
+
+#print(fruits) 
+#for fruit in fruits:
+# print(fruit)
