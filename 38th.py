8b01aea3ddc4dfef9fc07c31ed1f97c53d413df6
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-22 11:32:44 +0530

    along with exercise #impp

---
+++ 38th.py
@@ -0,0 +1,28 @@
+# module
+
+#import math
+#import math as m
+#from math import e
+
+#a, b, c, d,  = 1, 2, 3, 4, 5 
+
+#print(e ** a)
+#print(e ** b)
+#print(e ** c)
+#print(e ** d)
+#print(e ** e)
+
+import example
+
+result = example.pi
+result = example.square(3)
+result = example.cube(3)
+result = example.circumference(3)
+result = example.area(3)
+
+
+
+print(result)
+
+
+# print(help("modules"))
---
+++ example.py
@@ -0,0 +1,15 @@
+# example
+
+pi = 3.141159
+
+def square(x):
+    return x ** 2
+
+def cube(x):
+    return x ** 3
+
+def circumference(radius):
+    return 2 * pi * radius
+
+def area(radius):
+    return pi * radius ** 2
