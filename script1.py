b9442a6b374136e77a6a82d63adb1866d9dc10b7
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-24 11:42:31 +0530

    40th.py is here with two scripts.

---
+++ script1.py
@@ -0,0 +1,11 @@
+def favourite_car(car):
+    print(f"Your favourite car is {car}")
+
+def main():
+    print("This is script1")
+    favourite_car("BMW")
+    print("GoodBye!")
+
+if __name__ == '__main__':
+    main()
+    
---
+++ script2.py
@@ -0,0 +1,13 @@
+from script1 import *
+
+def favourite_drink(drink):
+    print(f"Your favourite drink is {drink}")
+
+print("This is scrript2")
+favourite_car("BMW")    
+favourite_drink("coffee")
+print('GoodBye!')
+
+if __name__ == '__main__':
+    main()
+    
