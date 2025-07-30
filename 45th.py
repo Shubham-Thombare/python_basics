50e4ae48b0dac2009ebaca14369ec000cde19665
Shubham-Thombare <shubhamthombare862@gmail.com>
2025-07-30 11:54:54 +0530

    object oriented program!

---
+++ 45th.py
@@ -0,0 +1,11 @@
+# python object oriented programming
+from car import Car
+
+car1 = Car("BMW", "2025", "Blue", "False" ) 
+car2 = Car("Tesla", "2025", "Red", "True")
+
+#car1.drive()
+#car1.stop()
+
+car1.describe()
+car2.describe()
